b9da461e5e03d10040f8906b30effe3ff9bd06c3
Pushkar Gupta <117958263+22push@users.noreply.github.com>
2023-08-24 01:38:46 +0530

    fvdsf

--- backend/routes/resourcesroute.js
+++ backend/routes/resourcesroute.js
@@ -1,7 +1,10 @@
 const resources = require("./../controllers/resourcescontroller");
-const authentication= require("./../controllers/authstudent");
+const authentication = require("./../controllers/authstudent");
 const express = require("express");
 const router = express.Router();
-router.route('/resources').get(authentication.protect,resources.getresources).post(resources.createresources)
-
+router
+  .route("/resources")
+  .get(resources.getresources)
+  .post(resources.createresources);
+//authentication.protect,
 module.exports = router;
