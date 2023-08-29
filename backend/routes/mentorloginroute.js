95f299c56a9371d27007d05054edc0f378e470a9
Pushkar Gupta <117958263+22push@users.noreply.github.com>
2023-08-29 21:07:15 +0530

    done

--- backend/routes/mentorloginroute.js
+++ backend/routes/mentorloginroute.js
@@ -11,5 +11,7 @@ router
     mentorsignupcontroller.creatementorsignup
   );
   router.route("/login").post(mentorsignupcontroller.login);
- 
+  router.route("/forgotpassword").post(mentorsignupcontroller.forgotPassword);
+  router.route("/verifycode").post(mentorsignupcontroller.verifycode);
+  router.route("/resetpassword/:token").patch(mentorsignupcontroller.resetPassword);
 module.exports = router;
